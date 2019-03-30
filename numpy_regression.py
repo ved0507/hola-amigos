72c4f215c3ffec4baf62d9b1795338af1872ec9f
vedavyas Burli <vedavyas.pb@gmail.com>
2019-03-31 00:47:26 +0530

    added new py script

---
+++ numpy_regression.py
@@ -0,0 +1,24 @@
+# Regression is a technique to understand the relationship between 
+# the variables and how they contribute.
+# often they are related to producing a particular outcome.
+
+
+# Linear regression  - linear variables
+
+# Single Variable Linear Regression is a technique used to model
+# the relationship between a single input independent variable 
+# and an output dependent variable using a linear model i.e a line.
+# Y=mX+c
+# Multi Variable Linear Regression where a model is created for the 
+# relationship between multiple independent input variables 
+# and an output dependent variable. 
+# Y=a_1*X1+a_2*X2+c
+
+# numpy - numerical python
+# python is slower in execution
+
+
+# numpy fastens python operations by converting repetative code to 
+# compiled form
+import numpy as np
+print "Numpy imported successfully"
