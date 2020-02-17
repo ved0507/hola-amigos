9d595899277e08cce2478ece8cca13a027223a59
vedavyas <37264167+ved0507@users.noreply.github.com>
2020-02-18 00:40:04 +0530

    Quick sorting updated
    Quick sorting updated

--- AllSorting.py
+++ AllSorting.py
@@ -50,3 +50,30 @@ print('sorted array')
 
 for i in range(len(A)):
     print(A[i],end=' ')
+
+
+###Quick Sorting -Divide and rule  algorithm
+
+#avarage  complexity O(nlogn)
+''' Divide and conquer 
+inplace
+unstable'''
+
+def Partition(arr,low,high)	:
+	i=(low - 1)
+	piv = arr[high]
+	for j in range(low,high):
+		if arr[j]<piv:
+			i=i+1
+			arr[i],arr[j]=arr[j],arr[i]
+	arr[i+1],arr[high]=arr[high],arr[i+1]
+def quicksort(arr,low,high)
+  if low<high:
+	pi = partition(arr,low,high)
+	quicksort(arr, low, pi-1)
+	quicksort(arr,pi+1,high)
+arr=[11,33,44,66,10,23]
+n=len(arr)
+quicksort(arr,0,n-1)
+for i in range(n):
+	print(arr[i])
